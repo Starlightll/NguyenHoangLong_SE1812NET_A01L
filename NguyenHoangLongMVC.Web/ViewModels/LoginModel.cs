6f32b352de90727f753b18b74148b46ddc8bcad7
NGUYEN HOANG LONG <145125551+Starlightll@users.noreply.github.com>
2025-02-24 02:45:40 +0700

    Add validation attributes to LoginModel properties
    Updated `LoginModel.cs` to include a namespace declaration and added `[Required]` attributes to `Username` and `Password` properties. The `Password` property now also has a `[DataType(DataType.Password)]` annotation for proper input handling in user interfaces.

--- NguyenHoangLongMVC.Web/ViewModels/LoginModel.cs
+++ NguyenHoangLongMVC.Web/ViewModels/LoginModel.cs
@@ -1,8 +1,14 @@
-﻿namespace NguyenHoangLongMVC.Web.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NguyenHoangLongMVC.Web.ViewModels
 {
     public class LoginModel
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
     }
 }
